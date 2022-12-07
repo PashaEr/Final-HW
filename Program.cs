0011480d0b10c8e271e42d16c452a21e45c8a0d8
Pavel Ershov <pasha_MV2011@mail.ru>
2022-12-07 15:52:54 +0300

    Добавил решение

--- Program.cs
+++ Program.cs
@@ -1,6 +1,30 @@
 ﻿// Написать программу, которая из имеющегося массива строк сформиркет массив из строк, 
-//длинна которых меньше,либо равна 3 символа.Пераоначальный массив можно ввести с клавиатуры, либо задать
+//длинна которых меньше,либо равна 3 символа.Первоначальный массив можно ввести с клавиатуры, либо задать
 // на старте выполнения алгоритма. При решении не рекомендуется пользоваться колллекциями,
 //лучше обойтись исключительно массивами.
 
 
+string[] array1 = new string[5] {"789", "23", "print", "level", "res"};
+string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+PrintArray(array2);
